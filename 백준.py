40a243a50abaf74780b76b8be30fd335facc154f
StartDeveloperKim <ktw550111@naver.com>
2022-08-02 23:03:26 +0900

    [백준] - 13305

--- 백준.py
+++ 백준.py
@@ -1,19 +1,14 @@
-import sys
-input=sys.stdin.readline
-
 n=int(input())
-info=[]
-for _ in range(n):
-    info.append(list(map(int, input().split())))
+distance=list(map(int, input().split()))
+oil=list(map(int, input().split()))
 
-info.sort(key=lambda x: (x[1], x[0]))
+price=oil[0]
+ans=price*distance[0]
 
-start_time=info[0][1]
-ans=1
-for i in range(1, n):
-    if info[i][0]>=start_time:
-        start_time=info[i][1]
-        ans+=1
-
-
+for i in range(1, n-1):
+    print(ans)
+    if oil[i]<price:
+        price=oil[i]
+    ans+=(price*distance[i])
 print(ans)
+        
