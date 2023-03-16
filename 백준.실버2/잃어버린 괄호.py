4eea95ccdfc05ee0bc8278431d7e68d52ef5eaaa
StartDeveloperKim <ktw550111@naver.com>
2023-03-16 15:48:12 +0900

    다시풀기. (성공) 잃어버린 괄호

--- 백준.실버2/잃어버린 괄호.py
+++ 백준.실버2/잃어버린 괄호.py
@@ -4,16 +4,16 @@
 ## 3. result 에는 '+' 연산자가 있는 수식과 일반 수가 연산된 결과들이 있다.
 ## 4. 첫번째 수를 시작으로 수를 순차적으로 빼면 된다.
 
-oper=input()
-opers=oper.split('-')
+exp=input()
+exp=exp.split('-')
 
 result=[]
-for oper in opers:
-    o=sum(list(map(int, oper.split('+'))))
-    result.append(o)
-
+for i in range(len(exp)):
+    result.append(sum(list(map(int, exp[i].split('+')))))
+        
 ans=result[0]
 for i in range(1, len(result)):
     ans-=result[i]
 print(ans)
+
 
