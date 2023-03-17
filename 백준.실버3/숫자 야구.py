3f12b67e34b19087b5789ca7ebdbaa6b44655ac6
StartDeveloperKim <ktw550111@naver.com>
2023-03-17 16:24:12 +0900

    해결실패. 숫자 야구(다시풀자)

---
+++ 백준.실버3/숫자 야구.py
@@ -0,0 +1,19 @@
+from itertools import permutations
+
+n=int(input())
+questions=list(map(int, input().split())) for _ in range(n)
+nums=list(permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+
+
+for ques in questions:
+    for i in range(len(num)):
+        strike, ball=0, 0
+
+        for j in range(3):
+            if str(ques[0])[j] == num[i][j]:
+                strike+=1
+            elif  
+    
+    
+
+
