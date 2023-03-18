162764385c76de6f69420ffd155cdd2ffda7bdb8
StartDeveloperKim <ktw550111@naver.com>
2023-03-18 22:17:49 +0900

    다시풀기. 숫자 야구

--- 백준.실버3/숫자 야구.py
+++ 백준.실버3/숫자 야구.py
@@ -1,19 +1,24 @@
 from itertools import permutations
 
 n=int(input())
-questions=list(map(int, input().split())) for _ in range(n)
+questions=[list(map(int, input().split())) for _ in range(n)]
 nums=list(permutations([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 
-
 for ques in questions:
-    for i in range(len(num)):
+    remove_cnt=0
+    for i in range(len(nums)):
         strike, ball=0, 0
+        i-=remove_cnt
 
+        number=list(str(ques[0]))
         for j in range(3):
-            if str(ques[0])[j] == num[i][j]:
+            if int(number[j]) == nums[i][j]:
                 strike+=1
-            elif  
-    
-    
+            elif int(number[j]) in nums[i]:
+                ball+=1
 
+        if ques[1]!=strike or ques[2]!=ball:
+            nums.remove(nums[i])
+            remove_cnt+=1
+print(len(nums))
 
